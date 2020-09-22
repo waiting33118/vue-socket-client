f0b9350c64f04083ce46c8d3d75132dc6e800b88
TonyChung <waiting33118@gmail.com>
2020-09-22 13:10:50 +0800

    feat: initial project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>HOME</h1>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import io from 'socket.io-client'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  created () {
+    const socket = io('https://140.124.71.226:3030')
+
+    socket.on('connect', () => {
+      socket.connected
+        ? console.log('connected!')
+        : console.log('connection failed!')
+
+      console.log(socket)
+    })
   }
 }
 </script>
